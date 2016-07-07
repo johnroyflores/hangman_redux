f8b27e1cd0e3a132a78835fd81155018ce931327
John Flores <johnroyflores@gmail.com>
2016-07-07 12:06:12 -0500

    Added boilerplate starter code

--- letter.js
+++ letter.js
@@ -6,3 +6,14 @@ var Word = function(){
     return str.substr(0, index) + chr + str.substr(index + 1);
 	}
 }
+
+var Letter = function(let) {
+	//make a charac property and set it to what you think makes second_instructor_demonstration
+
+	//make an appear property and set it to what makes sense
+
+	//make a letterRender property and set it to a function that does what you think makes sense
+
+};
+
+//export the Letter constructor here
